673d00562d574cad7b8ac60b369b5e5955f38402
Jordi Prats <jprats@systemadmin.es>
2017-10-31 12:21:01 +0100

    host order

--- manifests/client/host.pp
+++ manifests/client/host.pp
@@ -3,12 +3,13 @@ define openssh::client::host(
                               $description           = undef,
                               $gssapi_authentication = true,
                               $check_host_ip         = true,
+                              $order                 = '00',
                             ) {
   include ::openssh::client
 
   concat::fragment { "${openssh::params::ssh_config} host ${host} ${name}":
     target  => $openssh::params::ssh_config,
-    order   => '10',
+    order   => "10_${order}",
     content => template("${module_name}/ssh_config/host.erb"),
   }
 }
